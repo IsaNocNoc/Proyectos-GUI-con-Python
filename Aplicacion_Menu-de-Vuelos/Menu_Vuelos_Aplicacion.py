d6b3b5493b6b10bff840c5a16fc3f32ca7469ce5
IsaNocNoc <enoc200094@gmail.com>
2024-02-04 00:12:20 -0600

    novena edicion

--- Aplicacion_Menu-de-Vuelos/Menu_Vuelos_Aplicacion.py
+++ Aplicacion_Menu-de-Vuelos/Menu_Vuelos_Aplicacion.py
@@ -9,20 +9,21 @@ class MenuVuelosAplicacion (QDialog):
 
         self.ListaSeleccion=Ui_Dialog()
         self.ListaSeleccion.setupUi(self)
-
+        #conexion de seleccion de casilla a texto informativo
         self.ListaSeleccion.SL_PrimeraClase.toggled.connect(self.TextoInformativo)
         self.ListaSeleccion.SL_ClaseNegocio.toggled.connect(self.TextoInformativo)
         self.ListaSeleccion.SL_ClaseEconomico.toggled.connect(self.TextoInformativo)
         self.show()
-
+        #conexion de clic de boton a funcion de ejecucion del boton
         self.ListaSeleccion.Btn_Sleccion.clicked.connect(self.SeleccionBoton)
         self.show()
 
     def TextoInformativo(self):
         costo_vuelo=0
-
+        #revisa si la casilla ya ha sido seleccionada
         if self.ListaSeleccion.SL_PrimeraClase.isChecked()==True:
             costo_vuelo=190
+            #settext muestra el texto en el level
             self.ListaSeleccion.Texto_Info.setText('El costo de primera clase es {}'.format(costo_vuelo))
 
         if self.ListaSeleccion.SL_ClaseNegocio.isChecked()==True:
@@ -44,7 +45,7 @@ class MenuVuelosAplicacion (QDialog):
             self.ListaSeleccion.Texto_Output.setText('Usted escogio el vuelo de clase de economico\nBuen Viaje!')
 
 if __name__ == '__main__':
-    app=QApplication(sys.argv)
-    ejecucion=MenuVuelosAplicacion()
-    ejecucion.show()
-    sys.exit(app.exec_())
+    app=QApplication(sys.argv)#ejecuta el sistema en base a los argumentos dados
+    ejecucion=MenuVuelosAplicacion()#ejecuta la clase
+    ejecucion.show()#muestra la ejecucion del programa
+    sys.exit(app.exec_())#cierra el programa cuando se pulsa "x"
